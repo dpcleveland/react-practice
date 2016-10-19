2a9d66625304dca6d510fcaa25e6e62c00b9aca5
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-18 21:57:27 -0500

    Simple props example

--- reactstarter/src/app/index.js
+++ reactstarter/src/app/index.js
@@ -1,1 +1,25 @@
-document.write('it works!!! tada');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create component
+var TodoComponent = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <p>{this.props.mssg}</p>
+                <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
+                <p><strong>Cheese smell factor:</strong> {this.props.cheese.smellFactor}</p>
+                <p><strong>Cheese price:</strong> {this.props.cheese.price}</p>
+            </div>
+        );
+    }
+});
+
+var myCheese = {
+    name: 'Camembert',
+    smellFactor: 'Extreme',
+    price: '3.50'
+};
+
+// Put component into HTML page
+ReactDOM.render(<TodoComponent mssg="I like cheese" cheese={myCheese} />, document.getElementById('todo-wrapper'))

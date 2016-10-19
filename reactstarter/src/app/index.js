dc21efb8473f39c004c2f300d007ded44e16e5f1
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-18 22:03:11 -0500

    Basic state example

--- reactstarter/src/app/index.js
+++ reactstarter/src/app/index.js
@@ -3,23 +3,24 @@ var ReactDOM = require('react-dom');
 
 // Create component
 var TodoComponent = React.createClass({
+    getInitialState: function(){
+        return {
+            todos: ['wash up', 'eat', 'take a nap']
+        }
+    },
     render: function(){
         return(
-            <div>
-                <p>{this.props.mssg}</p>
-                <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
-                <p><strong>Cheese smell factor:</strong> {this.props.cheese.smellFactor}</p>
-                <p><strong>Cheese price:</strong> {this.props.cheese.price}</p>
+            <div id="todo-list">
+                <p>The busiest people have teh most leisure...</p>
+                <ul>
+                    <li>{this.state.todos[0]}</li>
+                    <li>{this.state.todos[1]}</li>
+                    <li>{this.state.todos[2]}</li>
+                </ul>
             </div>
         );
-    }
+    } // render
 });
 
-var myCheese = {
-    name: 'Camembert',
-    smellFactor: 'Extreme',
-    price: '3.50'
-};
-
 // Put component into HTML page
-ReactDOM.render(<TodoComponent mssg="I like cheese" cheese={myCheese} />, document.getElementById('todo-wrapper'))
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'))

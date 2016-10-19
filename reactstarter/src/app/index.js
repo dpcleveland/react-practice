daa2db1ed571c18f3355b2560982d994eade6b26
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-18 22:28:45 -0500

    Timer function state change

--- reactstarter/src/app/index.js
+++ reactstarter/src/app/index.js
@@ -5,13 +5,20 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
     getInitialState: function(){
         return {
-            todos: ['wash up', 'eat', 'take a nap']
+            todos: ['wash up', 'eat', 'take a nap'],
+            age: 30
         }
     },
     render: function(){
+        var ager = setTimeout(function(){
+            this.setState({
+                age: 35
+            });
+        }.bind(this), 5000);
         return(
             <div id="todo-list">
                 <p>The busiest people have teh most leisure...</p>
+                <p>{this.state.age}</p>
                 <ul>
                     <li>{this.state.todos[0]}</li>
                     <li>{this.state.todos[1]}</li>

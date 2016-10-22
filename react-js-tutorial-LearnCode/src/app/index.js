0e3e32ba714065c0686adba4f28e89a2bf510cad
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-22 17:42:52 -0500

    Header, Footer folder and file structure

--- react-js-tutorial-LearnCode/src/app/index.js
+++ react-js-tutorial-LearnCode/src/app/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Layout from './components/Layout'
 // require('../css/styles.scss');
 
-class Layout extends React.Component {
-    constructor(){
-        super();
-        this.name = "Daniel";
-    }
-    render(){
-        return(
-            <h1>It's {this.name}!</h1>
-        );
-    }
-}
 
 // Put component into HTML page
 const app = document.getElementById('app');

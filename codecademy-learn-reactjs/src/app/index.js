a85ad012f50c38950823ef70b315462e59f236a6
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-30 21:11:57 -0500

    Render function

--- codecademy-learn-reactjs/src/app/index.js
+++ codecademy-learn-reactjs/src/app/index.js
@@ -1,4 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var MyComponentClass = React.createClass({});
+var componentBlueprint = {
+    render: function(){
+        return <h1>Hello world</h1>
+    }
+};
+
+var MyComponentClass = React.createClass(componentBlueprint);

d5cc704ba2e3163ae8aeb5fd97d804b5ef17508b
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-03 21:32:01 -0500

    event listender in a component

--- codecademy-learn-reactjs/src/app/index.js
+++ codecademy-learn-reactjs/src/app/index.js
@@ -1,13 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var MyName = React.createClass({
-	// name property goes here:
-	name: 'whatever-your-name-is-goes-here',
+var Button = React.createClass({
+  scream: function () {
+    alert('AAAAAAAAHHH!!!!!');
+  },
 
   render: function () {
-    return <h1>My name is {this.name}.</h1>;
+    return <button onClick={this.scream}>AAAAAH!</button>;
   }
 });
 
-ReactDOM.render(<MyName />, document.getElementById('app'));
+ReactDOM.render(<Button />, document.getElementById('app'));

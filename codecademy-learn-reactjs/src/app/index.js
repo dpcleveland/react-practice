875c7e972da96daaea0c0e9b8e3ef87a02b8886e
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-05 21:13:31 -0500

    Pass props to component

--- codecademy-learn-reactjs/src/app/index.js
+++ codecademy-learn-reactjs/src/app/index.js
@@ -2,17 +2,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var PropsDisplayer = React.createClass({
-  render: function () {
-  	var stringProps = JSON.stringify(this.props);
+    render: function() {
+        var stringProps = JSON.stringify(this.props);
 
-    return (
-      <div>
-        <h1>CHECK OUT MY PROPS OBJECT</h1>
-        <h2>{stringProps}</h2>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h1>CHECK OUT MY PROPS OBJECT</h1>
+                <h2>{stringProps}</h2>
+            </div>
+        );
+    }
 });
 
 // ReactDOM.render goes here:
-ReactDOM.render(<PropsDisplayer />, document.getElementById('app'));
+ReactDOM.render(
+    <PropsDisplayer myProp="Hello"/>, document.getElementById('app'));

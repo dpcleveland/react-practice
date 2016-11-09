82684588168c1805ac10a97d206f3086a0fde850
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-08 23:07:49 -0600

    Made function an object property

--- codecademy-learn-reactjs/src/app/Talker.js
+++ codecademy-learn-reactjs/src/app/Talker.js
@@ -2,14 +2,13 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Button = require('./Button');
 
-function talk() {
-    for (var speech = '', i = 0; i < 10000; i++) {
-        speech += 'blah ';
-    }
-    alert(speech);
-}
-
 var Talker = React.createClass({
+    talk: function() {
+        for (var speech = '', i = 0; i < 10000; i++) {
+            speech += 'blah ';
+        }
+        alert(speech);
+    },
     render: function() {
         return <Button/>;
     }

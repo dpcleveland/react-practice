7d64cc863728023e6c5a77149983855b245a6ba8
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-15 17:56:34 -0600

    fix

--- codecademy-learn-reactjs/src/app/Parent.js
+++ codecademy-learn-reactjs/src/app/Parent.js
@@ -2,11 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
-var Parent = React.createClass(){
+var Parent = React.createClass({
 	getInitialState: function(){
 		return { name: 'Frarthur' };
 	},
 	render: function(){
 		return <div></div>;
 	}
-};
+});

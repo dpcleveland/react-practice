b428b0b7126bc9b1eaa9f8a76dc66f0d2964559f
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-16 17:50:42 -0600

    pass a component's state

--- codecademy-learn-reactjs/src/app/Parent.js
+++ codecademy-learn-reactjs/src/app/Parent.js
@@ -1,12 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var Child = require('./Child');
 
 var Parent = React.createClass({
 	getInitialState: function(){
 		return { name: 'Frarthur' };
 	},
 	render: function(){
-		return <div></div>;
+		return <Child name={this.state.name} />;
 	}
 });
+
+ReactDOM.render(<Parent />, document.getElementById('app'));

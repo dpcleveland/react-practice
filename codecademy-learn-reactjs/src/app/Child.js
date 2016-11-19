712433385fb8e4203c15944d5149975f31b092a8
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-18 22:28:58 -0600

    receive event handler

--- codecademy-learn-reactjs/src/app/Child.js
+++ codecademy-learn-reactjs/src/app/Child.js
@@ -1,13 +1,18 @@
 var React = require('react');
 
 var Child = React.createClass({
+  handleChange: function (e) {
+    var name = e.target.value;
+    this.props.onChange(name);
+  },
+  
   render: function () {
     return (
       <div>
         <h1>
           Hey my name is {this.props.name}!
         </h1>
-        <select id="great-names">
+        <select id="great-names" onChange={this.handleChange}>
           <option value="Frarthur">
             Frarthur
           </option>

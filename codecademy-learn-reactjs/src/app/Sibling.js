ae33354a59a78d3af52492c9dc8192452ddd0284
Daniel Cleveland <dpcleveland@gmail.com>
2016-11-19 23:50:19 -0600

    display information in sibling component

--- codecademy-learn-reactjs/src/app/Sibling.js
+++ codecademy-learn-reactjs/src/app/Sibling.js
@@ -2,12 +2,12 @@ var React = require('react');
 
 var Sibling = React.createClass({
   render: function () {
-
+	var name = this.props.name;
     return (
       <div>
-        <h1>Hey, my name is Frarthur!</h1>
-        <h2>Don't you think Frarthur is the prettiest name ever?</h2>
-        <h2>Sure am glad that my parents picked Frarthur!</h2>
+        <h1>Hey, my name is {name}!</h1>
+        <h2>Don't you think {name} is the prettiest name ever?</h2>
+        <h2>Sure am glad that my parents picked {name}!</h2>
       </div>
     );
   }

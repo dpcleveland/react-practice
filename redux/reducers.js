e21593895bc936045397d04c8c9ae8473690986e
Daniel Cleveland <dpcleveland@gmail.com>
2017-02-13 19:48:03 -0600

    Actions, reducers

---
+++ redux/reducers.js
@@ -0,0 +1,14 @@
+/*
+ *  reducers
+ */
+
+import { VisibilityFilters } from './actions'
+
+const initialState = {
+    visibilityFilter: VisibilityFilters.SHOW_ALL,
+    todos: []
+}
+
+function todoApp(state = initialState, action) {
+    return state
+}
